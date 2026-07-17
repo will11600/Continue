b5d986b7a7ef14d9c2a6fdfcef6fc5f91b6617b4
will11600 <wb11600@gmail.com>
2026-07-17 16:32:13 +0100

    Expand ContinueWith test coverage and add data providers
    Added NewResult struct and new test methods (ContinueWithTest11–20) to cover more ContinueWith overloads, including explicit generic parameters and various parameter combinations (scheduler, cancellation token, options). Introduced GatherFuncNewResultParameters and GatherTaskFuncParameters to supply test data for these scenarios.

--- tests/Continue/TaskExtensionsTests.cs
+++ tests/Continue/TaskExtensionsTests.cs
@@ -21,6 +21,8 @@ public class TaskExtensionsTests
 
     public readonly struct Result;
 
+    public readonly struct NewResult;
+
     [Theory, MemberData(nameof(GatherActionParameters), parameters: Parameters.All)]
     public void ContinueWithTest1(
         Task task,
@@ -79,7 +81,7 @@ public class TaskExtensionsTests
         State state,
         TaskScheduler scheduler)
     {
-        task.ContinueWith(continuationFunction, state, scheduler);
+        task.ContinueWith<Result, State>(continuationFunction, state, scheduler);
     }
 
     [Theory, MemberData(nameof(GatherFuncParameters), parameters: Parameters.All)]
@@ -91,7 +93,7 @@ public class TaskExtensionsTests
         TaskContinuationOptions continuationOptions,
         TaskScheduler scheduler)
     {
-        task.ContinueWith(continuationAction, state, cancellationToken, continuationOptions, scheduler);
+        task.ContinueWith<Result, State>(continuationAction, state, cancellationToken, continuationOptions, scheduler);
     }
 
     [Theory, MemberData(nameof(GatherFuncParameters), parameters: Parameters.CancellationToken)]
@@ -101,7 +103,7 @@ public class TaskExtensionsTests
         State state,
         CancellationToken cancellationToken)
     {
-        task.ContinueWith(continuationFunction, state, cancellationToken);
+        task.ContinueWith<Result, State>(continuationFunction, state, cancellationToken);
     }
 
     [Theory, MemberData(nameof(GatherFuncParameters), parameters: Parameters.ContinuationOptions)]
@@ -111,7 +113,7 @@ public class TaskExtensionsTests
         State state,
         TaskContinuationOptions continuationOptions)
     {
-        task.ContinueWith(continuationFunction, state, continuationOptions);
+        task.ContinueWith<Result, State>(continuationFunction, state, continuationOptions);
     }
 
     [Theory, MemberData(nameof(GatherFuncParameters), parameters: Parameters.None)]
@@ -119,6 +121,109 @@ public class TaskExtensionsTests
         Task<Result> task,
         Func<Task<Result>, State, Result> continuationFunction,
         State state)
+    {
+        task.ContinueWith<Result, State>(continuationFunction, state);
+    }
+
+    [Theory, MemberData(nameof(GatherFuncNewResultParameters), parameters: Parameters.Scheduler)]
+    public void ContinueWithTest11(
+        Task<Result> task,
+        Func<Task<Result>, State, NewResult> continuationFunction,
+        State state,
+        TaskScheduler scheduler)
+    {
+        task.ContinueWith(continuationFunction, state, scheduler);
+    }
+
+    [Theory, MemberData(nameof(GatherFuncNewResultParameters), parameters: Parameters.All)]
+    public void ContinueWithTest12(
+        Task<Result> task,
+        Func<Task<Result>, State, NewResult> continuationFunction,
+        State state,
+        CancellationToken cancellationToken,
+        TaskContinuationOptions continuationOptions,
+        TaskScheduler scheduler)
+    {
+        task.ContinueWith(continuationFunction, state, cancellationToken, continuationOptions, scheduler);
+    }
+
+    [Theory, MemberData(nameof(GatherFuncNewResultParameters), parameters: Parameters.CancellationToken)]
+    public void ContinueWithTest13(
+        Task<Result> task,
+        Func<Task<Result>, State, NewResult> continuationFunction,
+        State state,
+        CancellationToken cancellationToken)
+    {
+        task.ContinueWith(continuationFunction, state, cancellationToken);
+    }
+
+    [Theory, MemberData(nameof(GatherFuncNewResultParameters), parameters: Parameters.ContinuationOptions)]
+    public void ContinueWithTest14(
+        Task<Result> task,
+        Func<Task<Result>, State, NewResult> continuationFunction,
+        State state,
+        TaskContinuationOptions continuationOptions)
+    {
+        task.ContinueWith(continuationFunction, state, continuationOptions);
+    }
+
+    [Theory, MemberData(nameof(GatherFuncNewResultParameters), parameters: Parameters.None)]
+    public void ContinueWithTest15(
+        Task<Result> task,
+        Func<Task<Result>, State, NewResult> continuationFunction,
+        State state)
+    {
+        task.ContinueWith(continuationFunction, state);
+    }
+
+    [Theory, MemberData(nameof(GatherTaskFuncParameters), parameters: Parameters.All)]
+    public void ContinueWithTest16(
+        Task task,
+        Func<Task, State, Result> continuationFunction,
+        State state,
+        CancellationToken cancellationToken,
+        TaskContinuationOptions continuationOptions,
+        TaskScheduler scheduler)
+    {
+        task.ContinueWith(continuationFunction, state, cancellationToken, continuationOptions, scheduler);
+    }
+
+    [Theory, MemberData(nameof(GatherTaskFuncParameters), parameters: Parameters.CancellationToken)]
+    public void ContinueWithTest17(
+        Task task,
+        Func<Task, State, Result> continuationFunction,
+        State state,
+        CancellationToken cancellationToken)
+    {
+        task.ContinueWith(continuationFunction, state, cancellationToken);
+    }
+
+    [Theory, MemberData(nameof(GatherTaskFuncParameters), parameters: Parameters.ContinuationOptions)]
+    public void ContinueWithTest18(
+        Task task,
+        Func<Task, State, Result> continuationFunction,
+        State state,
+        TaskContinuationOptions continuationOptions)
+    {
+        task.ContinueWith(continuationFunction, state, continuationOptions);
+    }
+
+    [Theory, MemberData(nameof(GatherTaskFuncParameters), parameters: Parameters.Scheduler)]
+    public void ContinueWithTest19(
+        Task task,
+        Func<Task, State, Result> continuationFunction,
+        State state,
+        TaskScheduler scheduler)
+    {
+        task.ContinueWith(continuationFunction, state, scheduler);
+    }
+
+    [Theory, MemberData(nameof(GatherTaskFuncParameters), parameters: Parameters.None)]
+    public void ContinueWithTest20(
+        Task task,
+        Func<Task, State, Result> continuationFunction,
+        State state)
+
     {
         task.ContinueWith(continuationFunction, state);
     }
@@ -147,5 +252,15 @@ public class TaskExtensionsTests
     public static IEnumerable<object[]> GatherFuncParameters(Parameters parameters)
     {
         yield return [Task.FromResult(default(Result)), (Func<Task<Result>, State, Result>)((t, s) => default), new State(), .. GenerateParameters(parameters)];
+    }
+
+    public static IEnumerable<object[]> GatherFuncNewResultParameters(Parameters parameters)
+    {
+        yield return [Task.FromResult(default(Result)), (Func<Task<Result>, State, NewResult>)((t, s) => default), new State(), .. GenerateParameters(parameters)];
+    }
+
+    public static IEnumerable<object[]> GatherTaskFuncParameters(Parameters parameters)
+    {
+        yield return [Task.CompletedTask, (Func<Task, State, Result>)((t, s) => default), new State(), .. GenerateParameters(parameters)];
     }
 }
